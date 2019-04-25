53ab53dab11ea4bf0f2c4f026dec1f6fdb53c322
vraghava <viswanath.raghava@amadeus.com>
2019-04-25 10:08:54 -0400

    second commit

---
+++ src/main/java/com/example/drools/spring/SpringDroolsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.drools.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDroolsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDroolsExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kie/services/DroolTestWithKie.java
@@ -0,0 +1,37 @@
+package com.example.kie.services;
+
+import com.example.drools.model.Product;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+
+
+public class DroolTestWithKie {
+
+    public static final void main(String[] args) {
+        try {
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+//Get the session named kseesion-rule that we defined in kmodule.xml above.
+//Also by default the session returned is always stateful.
+            KieSession kSession = kContainer.newKieSession("ksession-rule");
+
+            Product product = new Product();
+            product.setType("gold");
+
+            FactHandle fact1;
+
+            fact1 = kSession.insert(product);
+            kSession.fireAllRules();
+
+            System.out.println("The discount for the jewellery product "
+                    + product.getType() + " is " + product.getDiscount());
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
